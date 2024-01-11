61e37a27287dde7db5a1d2cfbaea4c4a629b88ee
Dorian <dubois.dorian2@gmail.com>
2024-01-11 13:24:39 +0100

    Suppression méthode register, insertion role admin au lancement de l'app

--- src/main/java/www/exam/janvier/Main.java
+++ src/main/java/www/exam/janvier/Main.java
@@ -6,7 +6,11 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import www.exam.janvier.entity.RoleEntity;
+import www.exam.janvier.entity.SocieteEntity;
 import www.exam.janvier.repository.RoleRepository;
+import www.exam.janvier.repository.SocieteRepository;
+
+import java.util.HashSet;
 
 
 @SpringBootApplication
@@ -18,7 +22,7 @@ public class Main {
     }
 
     @Bean
-    public CommandLineRunner initData(RoleRepository roleRepository) {
+    public CommandLineRunner initData(RoleRepository roleRepository, SocieteRepository societeRepository) {
         return args -> {
             if (roleRepository.findByName("ROLE_CLIENT") == null) {
                 RoleEntity roleClient = new RoleEntity();
@@ -29,6 +33,17 @@ public class Main {
                 RoleEntity roleAdmin = new RoleEntity();
                 roleAdmin.setName("ROLE_ADMIN");
                 roleRepository.save(roleAdmin);
+            }
+            if(societeRepository.findByNomSociete("admin")==null){
+                SocieteEntity admin = new SocieteEntity();
+                admin.setNomSociete("admin");
+                admin.setMail("admin@admin.com");
+                admin.setPassword("$2a$10$Pfj/3gLgpCMC6oaZ3ToafeylN8BE.jFWnlifP745tIiIAK5bCu7AW");
+                RoleEntity roleAdmin = roleRepository.findByName("ROLE_ADMIN");
+                admin.setRoles(new HashSet<>());
+                admin.getRoles().add(roleAdmin);
+                admin.setProduits(new HashSet<>());
+                societeRepository.save(admin);
             }
         };
     }
